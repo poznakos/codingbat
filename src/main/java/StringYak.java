9aff2377cc2c0976eb5f74d52b6e526b86087930
konradpoznakowski <kpoznakowski@gmail.com>
2023-07-23 21:15:00 +0200

    Array667

--- src/main/java/StringYak.java
+++ src/main/java/StringYak.java
@@ -1,6 +1,5 @@
 public class StringYak {
     public String stringYak(String str) {
-        String result = "";
         String whithaoutYak = str;
         for (int i = 0; i < whithaoutYak.length()-2; i++) {
             String text = whithaoutYak.substring(i, i+3);
