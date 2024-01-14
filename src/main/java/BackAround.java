aa55a6e1ddfb04de24f6a85f38189cb8247d333b
konradpoznakowski <kpoznakowski@gmail.com>
2024-01-14 17:14:06 +0100

    BackAround

--- src/main/java/BackAround.java
+++ src/main/java/BackAround.java
@@ -1,11 +1,15 @@
 public class BackAround {
-    public String backAround(String word) {
-        String last;
-
-        last = word.substring(word.length() - 1, word.length());
-
-        return last  + word + last;
+    public String backAround(String str) {
+        String last = str.substring(str.length() - 1);
+        return last + str + last;
+//        String last;
+//
+//        last = word.substring(word.length() - 1, word.length());
+//
+//        return last + word + last;
+//        last two word;
 
     }
+
 
 }
